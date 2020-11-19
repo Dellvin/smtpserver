bb6aeac0fe55a194b435b0ad1be14bc4b0810563
Dellvin <dellvin.black@gmail.com>
2020-11-19 20:44:41 +0300

    add recover

--- main.go
+++ main.go
@@ -29,11 +29,21 @@ func (bkd *Backend) AnonymousLogin(state *smtp.ConnectionState) (smtp.Session, e
 type Session struct{}
 
 func (s *Session) Mail(from string, opts smtp.MailOptions) error {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in mail", r)
+		}
+	}()
 	fmt.Println("Mail from:", from)
 	return nil
 }
 
 func (s *Session) Rcpt(to string) error {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in rcpt", r)
+		}
+	}()
 	fmt.Println("Rcpt to:", to)
 	return nil
 }
