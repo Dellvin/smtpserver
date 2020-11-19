0a5c4748e91598984e52d91927921ae8c03e5145
Dellvin <dellvin.black@gmail.com>
2020-11-19 20:48:31 +0300

    add more recover

--- main.go
+++ main.go
@@ -65,6 +65,11 @@ func (s *Session) Logout() error {
 }
 
 func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in rcpt", r)
+		}
+	}()
 	be := &Backend{}
 
 	s := smtp.NewServer(be)
